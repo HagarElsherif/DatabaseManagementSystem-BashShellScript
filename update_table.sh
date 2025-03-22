c118b8ee35e4ebb8f944e6468eff12137936fe82
hagar <hagar@localhost.localdomain>
2025-03-22 16:02:04 +0200

    Fix: unique id and datatypes validations

--- update_table.sh
+++ update_table.sh
@@ -4,23 +4,58 @@
 
 DB_DIR="dbms/$1"
 
-
 echo ""
 read -p "Enter the name of the table: " table_name
 table_file="$DB_DIR/${table_name}_metadata.txt"
 table_data="$DB_DIR/$table_name.txt"
 
-
+# Check if table exists
 if [ ! -f "$table_file" ]; then
     echo "Table does not exist."
     exit 1
 fi
 
-# Read column names
+# Read column names and types from data
 cols_names=$(sed -n '3,$p' "$table_file" | cut -d: -f1)
+cols_types=$(sed -n '3,$p' "$table_file" | cut -d: -f2)
 readarray -t cols_names_array <<< "$cols_names"
+readarray -t cols_types_array <<< "$cols_types"
 
-# Ask for Primary Key value
+# Function to check integer 
+check_int(){
+  if [[ $1 =~ ^[0-9]+$ ]]; then
+    return 0
+  else
+    echo -e "Invalid Integer \n"
+    return 1
+   fi
+}
+
+# Function to check string 
+check_string(){
+    if [[ -z $1 ]]; then
+        echo -e "Error: Empty input\n"
+        return 1
+    fi
+
+    if [[ $1 =~ ^[a-zA-Z_][a-zA-Z0-9_\ ]*$ ]]; then
+        return 0
+    else
+        echo -e "Invalid string format\n"
+        return 1
+    fi
+}
+
+# ensure Primary Key is unique
+check_PK(){
+    awk -v value=$1 '
+    BEGIN{ FS=":" }
+    { if ($1 == value) { print "The primary key must be unique"; exit 1 } }
+    END{}
+    ' $table_data
+}
+
+
 read -p "Enter the Primary Key value to update: " pk_value
 
 # Find the row containing the Primary Key
@@ -31,13 +66,12 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Display column options
+# Display columns
 echo "Select the column to update:"
 for i in "${!cols_names_array[@]}"; do
     echo "$((i+1))) ${cols_names_array[i]}"
 done
 
-# Get column choice
 read -p "Enter column number: " col_num
 
 # Validate column number
@@ -46,10 +80,25 @@ if [[ ! $col_num =~ ^[0-9]+$ ]] || ((col_num < 1 || col_num > ${#cols_names_arra
     exit 1
 fi
 
-# Ask for new value
-read -p "Enter new value: " new_value
 
-# Update the row
+while true; do
+    read -p "Enter new value: " new_value
+
+    # Validate input based on type
+    data_type=${cols_types_array[col_num-1]}
+
+    if [ "$col_num" -eq 1 ]; then
+        check_PK "$new_value" || continue
+    fi
+
+    if [ "$data_type" = "int" ]; then
+        check_int "$new_value" && break
+    else
+        check_string "$new_value" && break
+    fi
+done
+
+
 awk -F: -v pk="$pk_value" -v col="$col_num" -v new="$new_value" '
 BEGIN {OFS=":"}
 $1 == pk { $col = new }
